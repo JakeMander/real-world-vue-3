eaa5d50cb99d7f2494690fd2f4f25b5eb13f8dc8
Jake Mander <Jake@chilli-is.co.uk>
2021-05-14 18:01:07 +0100

    "Completed single file componenets"

--- real-world-vue/src/App.vue
+++ real-world-vue/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <h1>Jakes Do Good Stuff Site</h1>
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -26,5 +27,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
